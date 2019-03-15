99367e2160e1f8f84b49371ac2008700ca067e5f
caixiaobing <caixiaobing@foresee.com.cn>
2019-03-15 11:16:52 +0800

    utils

---
+++ packages/ttk-utils/src/index.js
@@ -0,0 +1,27 @@
+import fetch from './fetch'
+import path from './path'
+import expression from './expression'
+import string from './string'
+import exception from './exception'
+import navigate from './navigate'
+import {getGlobal} from './env'
+
+export default {
+	fetch,
+	string,
+	path,
+	expression,
+	exception,
+	navigate,
+	getGlobal
+}
+
+export {
+	fetch,
+	string,
+	path,
+	expression,
+	exception,
+	navigate,
+	getGlobal
+}
